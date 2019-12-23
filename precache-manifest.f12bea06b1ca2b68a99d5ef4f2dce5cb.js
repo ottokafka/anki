d2273eae62206221ca9e37fd66341c2907ef9f29
otto kafka <kafka@ottos-MacBook-Pro.local>
2019-12-23 18:22:22 +0800

    Updates

---
+++ precache-manifest.f12bea06b1ca2b68a99d5ef4f2dce5cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b404585e3fcda88a589c6860689ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a22e8aeccd4a386f8c4",
+    "url": "/static/css/main.95872c97.chunk.css"
+  },
+  {
+    "revision": "8c1dfcfba3340da13510",
+    "url": "/static/js/2.ac605257.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ac605257.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a22e8aeccd4a386f8c4",
+    "url": "/static/js/main.d3a05278.chunk.js"
+  },
+  {
+    "revision": "d3451e09a8b9dbaba5de",
+    "url": "/static/js/runtime-main.8d66e8ba.js"
+  }
+]);
